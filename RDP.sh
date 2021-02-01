491a6636e0b48aa691f31a573ad360d08ece6191
VISHAL KUMAR SINGH <54959304+vishalkumarsingh999@users.noreply.github.com>
2021-02-01 17:05:31 +0530

    Update RDP.sh

--- RDP.sh
+++ RDP.sh
@@ -3,7 +3,7 @@ printf "Installing RDP Be Patience... " >&2
 {
 sudo useradd -m ALOK
 sudo adduser VISHAL sudo
-echo 'VISHAL:8426' | sudo chpasswd
+echo 'VISHAL:0000' | sudo chpasswd
 sed -i 's/\/bin\/sh/\/bin\/bash/g' /etc/passwd
 sudo apt-get update
 wget https://dl.google.com/linux/direct/chrome-remote-desktop_current_amd64.deb
